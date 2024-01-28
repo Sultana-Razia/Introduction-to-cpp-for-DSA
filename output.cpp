3e63b7773e2548792545c0230cbe78c130e20315
Sultana Razia <sultanarazia483@gmail.com>
2024-01-28 16:26:02 +0600

    module 1

---
+++ output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World\n";
+    std::cout << "Hello\n";
+    std::cout << "Hello "
+              << "World\n";
+    std::cout << "Hello" << std::endl
+              << "World\n";
+    int a = 100, b = 200;
+    std::cout << a << b << std::endl;
+    std::cout << a << " " << b << endl;
+    cout << a;
+    return 0;
+}
---
+++ swap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void my_swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    my_swap(&a, &b);
+    cout << a << " " << b << endl;
+    return 0;
+}
